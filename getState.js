05ac1e86a028bdc5d2842bb56c349d59aa240551
Hoeyeon <shy9546@naver.com>
2020-05-14 22:03:22 +0900

    feat: set score

--- getState.js
+++ getState.js
@@ -6,34 +6,48 @@ const setUserHand = () => {
   const buttons = document.querySelectorAll("button");
   const userImg = document.querySelector(".user_hand");
   for (let i = 0; i < buttons.length; i++) {
-    console.log(buttons[i]);
     buttons[i].addEventListener("click", (event) => {
-      console.log(`${buttons[i].innerText} Clicked!`);
       userHand = buttons[i].innerText;
       userImg.src = `assets/${userHand}.png`;
-      computerHand = shuffleHand();
+      computerHand = setcomputerHand();
+      const result = compare(userHand, computerHand);
+      setScore(result);
     });
   }
 };
-function delay() {
-  return new Promise((resolve) => setTimeout(resolve, 150));
-}
-
-const shuffleHand = async () => {
-  const img = document.querySelector(".computer_hand");
-  let computerHand = "";
-  for (let i = 0; i < 25; i++) {
-    computerHand = await setcomputerHand();
-    console.log(computerHand);
-    img.src = `assets/${computerHand}.png`;
+const setScore = (result) => {
+  const who = document.querySelector(".who_win");
+  const userScore = document.querySelector(".user_score");
+  const computerScore = document.querySelector(".computer_score");
+  if (result === 0) who.innerText = "DRAW!";
+  else if (result === 1) {
+    who.innerText = "Player Win!";
+    userScore.innerText++;
+  } else {
+    who.innerText = "Computer Win!";
+    computerScore.innerText++;
   }
+};
+const compare = (userHand, computerHand) => {
+  if (userHand === computerHand) {
+    console.log("DRAW!");
+    return 0;
+  } else {
+    if (userHand === "ROCK" && computerHand === "SCISSORS") return 1;
+    else if (userHand === "SCISSORS" && computerHand === "PAPER") return 1;
+    else if (userHand === "PAPER" && computerHand === "ROCK") return 1;
+    else return -1;
+  }
+};
+
+const setcomputerHand = () => {
+  const img = document.querySelector(".computer_hand");
+  const computerHand = randomChoice();
+  img.src = `assets/${computerHand}.png`;
   return computerHand;
 };
 
-const setcomputerHand = async () => {
-  await delay();
-  return hands[Math.floor(Math.random() * hands.length)];
-};
+const randomChoice = () => hands[Math.floor(Math.random() * hands.length)];
 
 const init = () => {
   setUserHand();
