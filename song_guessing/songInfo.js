10d7498329b5acc166f460aa65b7d6a85ba3ad76
Hoeyeon <shy9546@naver.com>
2020-06-10 22:49:15 +0900

    feat: song_guess UI

---
+++ song_guessing/songInfo.js
@@ -0,0 +1,41 @@
+function LightenDarkenColor(col, amt) {
+  var usePound = false;
+
+  if (col[0] == "#") {
+    col = col.slice(1);
+    usePound = true;
+  }
+
+  var num = parseInt(col, 16);
+
+  var r = (num >> 16) + amt;
+
+  if (r > 255) r = 255;
+  else if (r < 0) r = 0;
+
+  var b = ((num >> 8) & 0x00ff) + amt;
+
+  if (b > 255) b = 255;
+  else if (b < 0) b = 0;
+
+  var g = (num & 0x0000ff) + amt;
+
+  if (g > 255) g = 255;
+  else if (g < 0) g = 0;
+
+  return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
+}
+
+const MUSIC_PATH = "https://www.youtube.com/embed/";
+const THUMBNAIL = "http://img.youtube.com/vi/";
+const musicKey = "2GRP1rkE4O0";
+
+const thumbnail = document.querySelector(".image");
+const video = document.querySelector("iframe");
+thumbnail.src = `${THUMBNAIL}${musicKey}/0.jpg`;
+
+const buttonWrapper = document.querySelector(".button-wrapper");
+buttonWrapper.style.background = `linear-gradient(45deg, ${LightenDarkenColor(
+  "#fd79a8",
+  30
+)},#ffffff)`;
